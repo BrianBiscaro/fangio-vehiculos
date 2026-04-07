3abd57c76be8ad8df01185316d3f14e86bcf8f3a
Brian Biscaro <brianbiscaro22@gmail.com>
2026-04-06 21:35:05 -0300

    ft(auth): creo AuthResponse

---
+++ backend/src/main/java/com/fangio/backend/auth/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.fangio.backend.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token, String username, String email){
+        
+    }
+}
