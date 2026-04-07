a0377f4e50893b94c51f5cd434da7b7f59193856
Brian Biscaro <brianbiscaro22@gmail.com>
2026-04-07 20:52:08 -0300

    ft(auth): fix imports

--- backend/src/main/java/com/fangio/backend/auth/AuthService.java
+++ backend/src/main/java/com/fangio/backend/auth/AuthService.java
@@ -3,6 +3,7 @@ package com.fangio.backend.auth;
 import org.springframework.context.support.BeanDefinitionDsl.Role;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
