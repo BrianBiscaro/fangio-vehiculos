9020f96350bc49f7c48e6c49ba83522ff82d3145
Brian Biscaro <brianbiscaro22@gmail.com>
2026-04-07 23:02:54 -0300

    ft(auth): Creo clase user principal

--- backend/src/main/java/com/fangio/backend/security/UserPrincipal.java
+++ backend/src/main/java/com/fangio/backend/security/UserPrincipal.java
@@ -1,5 +1,38 @@
 package com.fangio.backend.security;
 
-public class UserPrincipal {
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fangio.backend.auth.User;
+
+public class UserPrincipal implements UserDetails {
     
+
+    private final User user;
+
+    public UserPrincipal(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+       return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+       return user.getUsername();
+    }
+    
+
 }
