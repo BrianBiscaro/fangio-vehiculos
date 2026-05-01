68461c1ec71f397549a64e85bb7a58761a2c28ed
Brian Biscaro <brianbiscaro22@gmail.com>
2026-04-30 21:47:14 -0300

    Agrego Cliente Package

---
+++ backend/src/main/java/com/fangio/backend/cliente/Cliente.java
@@ -0,0 +1,10 @@
+package com.fangio.backend.cliente;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+}
--- backend/src/main/java/com/fangio/backend/reserva/Reserva.java
+++ backend/src/main/java/com/fangio/backend/reserva/Reserva.java
@@ -1,6 +1,23 @@
 package com.fangio.backend.reserva;
 
+import com.fangio.backend.cliente.Cliente;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservas")
 public class Reserva {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Cliente cliente; // Relaciona Reserva con Cliente
+
     // Relaciona Vehiculo con Instructor
+
 }
